57f5c4d4d65e6a6753d040abea839091a337f769
z-seni <35400507+z-seni@users.noreply.github.com>
2023-05-22 20:14:01 +0200

    added RESTful api request sending created data structure for api send data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -61,14 +62,29 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  List<double> _accelerometerValues = [0, 0, 0];
-  List<double> _userAccelerometerValues = [0, 0, 0];
-  List<double> _gyroscopeValues = [0, 0, 0];
-  List<double> _magnetometerValues = [0, 0, 0];
-  LocationData? _currentLocation;
+  int _user = 0; //update on login
+  LocationData? _startLocation;
+  LocationData? _endLocation;
+  List<double> _accelerometerListX = [];
+  List<double> _accelerometerListY = [];
+  List<double> _accelerometerListZ = [];
+  List<double> _userAccelerometerListX = [];
+  List<double> _userAccelerometerListY = [];
+  List<double> _userAccelerometerListZ = [];
+  List<double> _gyroscopeListX = [];
+  List<double> _gyroscopeListY = [];
+  List<double> _gyroscopeListZ = [];
+  List<double> _magnetometerList = [];
+  List<double> _accelerometerValues = [0,0,0];
+  List<double> _userAccelerometerValues = [0,0,0];
+  List<double> _gyroscopeValues = [0,0,0];
+  List<double> _magnetometerValues = [0,0,0];
   int _lightIntensity = 0;
   late ThemeNotifier _themeNotifier;
   late Light _light;
+  int _readCount = 0;
+
+  Map<String, dynamic> _readData = { };
 
 
 
@@ -79,6 +95,8 @@ class _MyHomePageState extends State<MyHomePage> {
     _initLightSensor();
     _initSensors();
     _getLocation();
+    Timer.periodic(Duration(seconds: 1), (Timer t) => readData());
+
   }
 
   void _initLightSensor() async {
@@ -143,17 +161,126 @@ class _MyHomePageState extends State<MyHomePage> {
 
     _locationData = await location.getLocation();
     setState(() {
-      _currentLocation = _locationData;
+      _startLocation = _locationData;
     });
   }
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
+    // sendCaptureData();
   }
+
+  // Pošiljanje podatkov:
+  // vsakih 10s, 2x lokacija vsakih 5s
+  // vsako sekundo zajameš vse senzorje in dodas v array
+  // 'capturedBy' : Number,
+  // 	'latitude_start' : Number,
+  // 	'longitude_start' : Number,
+  // 	'latitude_end' : Number,
+  // 	'longitude_end' : Number,
+  // 	'captureDate' : Date,
+  // 	'accelerometerX': [Number],
+  // 	'accelerometerY': [Number],
+  // 	'accelerometerZ': [Number],
+  // 	'userAccelerometerX': [Number],
+  // 	'userAccelerometerY': [Number],
+  // 	'userAccelerometerZ': [Number],
+  // 	'gyroscopeX': [Number],
+  // 	'gyroscopeY': [Number],
+  // 	'gyroscopeZ': [Number],
+  // 	'lightIntensity': Number,
+  // 	'roadQuality': Number
+
+  Future<void> sendCaptureData() async {
+    // Define the capture data to send
+
+    print("Sending!!!");
+    // Convert capture data to JSON
+    String jsonData = jsonEncode(_readData);
+    // String jsonData = jsonEncode(_readData);
+
+    try {
+      // Send a POST request to the API endpoint
+      final response = await http.post(
+        Uri.parse('http://10.0.2.2:3001/phoneData'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonData,
+      );
+
+      if (response.statusCode == 201) {
+        // Capture data successfully sent
+        print('Capture data sent successfully');
+      } else {
+        // Handle error if capture data sending failed
+        print('Failed to send capture data: ${response.statusCode}');
+      }
+    } catch (error) {
+      // Handle any exceptions or network errors
+      print('Error sending capture data: $error');
+    }
+  }
+
   void _updateTheme() {
     bool isDarkMode = _lightIntensity < 50.0;
     _themeNotifier.updateTheme(isDarkMode);
+  }
+
+  void readData(){
+    if(_readCount == 5){
+      _getLocation();
+      _endLocation = _startLocation;
+    }else if(_readCount == 10){
+      _getLocation();
+      //send + null
+
+      _readData = {
+      'capturedBy': _user,
+      'latitude_start': _endLocation?.latitude,
+      'longitude_start': _endLocation?.longitude,
+      'latitude_end': _startLocation?.latitude,
+      'longitude_end': _startLocation?.longitude,
+      'captureDate': DateTime.now().toIso8601String(),
+      'accelerometerX': _accelerometerListX,
+      'accelerometerY': _accelerometerListY,
+      'accelerometerZ': _accelerometerListZ,
+      'userAccelerometerX': _userAccelerometerListX,
+      'userAccelerometerY': _userAccelerometerListY,
+      'userAccelerometerZ': _userAccelerometerListZ,
+      'gyroscopeX': _gyroscopeListX,
+      'gyroscopeY': _gyroscopeListY,
+      'gyroscopeZ': _gyroscopeListZ,
+      'lightIntensity': _lightIntensity,
+    };
+
+      sendCaptureData();
+
+      _accelerometerListX = [];
+      _accelerometerListY = [];
+      _accelerometerListZ = [];
+      _userAccelerometerListX = [];
+      _userAccelerometerListY = [];
+      _userAccelerometerListZ = [];
+      _gyroscopeListX = [];
+      _gyroscopeListY = [];
+      _gyroscopeListZ = [];
+      setState(() {  });
+      _readCount = 0;
+
+    }else{
+      _accelerometerListX.add(_accelerometerValues[0]);
+      _accelerometerListY.add(_accelerometerValues[1]);
+      _accelerometerListZ.add(_accelerometerValues[2]);
+      _userAccelerometerListX.add(_userAccelerometerValues[0]);
+      _userAccelerometerListY.add(_userAccelerometerValues[1]);
+      _userAccelerometerListZ.add(_userAccelerometerValues[2]);
+      _gyroscopeListX.add(_gyroscopeValues[0]);
+      _gyroscopeListY.add(_gyroscopeValues[1]);
+      _gyroscopeListZ.add(_gyroscopeValues[2]);
+
+    }
+    print("Read count:$_readCount");
+    _readCount++;
   }
 
 
@@ -161,6 +288,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     _themeNotifier = Provider.of<ThemeNotifier>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -169,41 +297,41 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline6,
-            ),
+            // const Text(
+            //   'You have pushed the button this many times:',
+            // ),
+            // Text(
+            //   '$_counter',
+            // ),
             SizedBox(height: 16.0),
-            Text('Accelerometer Data'),
-            Text('X: ${_accelerometerValues[0]}'),
-            Text('Y: ${_accelerometerValues[1]}'),
-            Text('Z: ${_accelerometerValues[2]}'),
-            SizedBox(height: 16.0),
-            Text('User Accelerometer Data'),
-            Text('X: ${_userAccelerometerValues[0]}'),
-            Text('Y: ${_userAccelerometerValues[1]}'),
-            Text('Z: ${_userAccelerometerValues[2]}'),
-            SizedBox(height: 16.0),
-            Text('Gyroscope Data'),
-            Text('X: ${_gyroscopeValues[0]}'),
-            Text('Y: ${_gyroscopeValues[1]}'),
-            Text('Z: ${_gyroscopeValues[2]}'),
-            SizedBox(height: 16.0),
-            Text('Magnetometer Data'),
-            Text('X: ${_magnetometerValues[0]}'),
-            Text('Y: ${_magnetometerValues[1]}'),
-            Text('Z: ${_magnetometerValues[2]}'),
-            SizedBox(height: 16.0),
-            if (_currentLocation != null) ...[
-              Text('Latitude: ${_currentLocation!.latitude}'),
-              Text('Longitude: ${_currentLocation!.longitude}'),
-            ] else
-              Text('Location data unavailable'),
-            SizedBox(height: 16.0),
-            Text('Light Intensity: $_lightIntensity'),
+            // Text(_readData.toString())
+            // Text('Accelerometer Data'),
+            // Text('X: ${_accelerometerValues[0]}'),
+            // Text('Y: ${_accelerometerValues[1]}'),
+            // Text('Z: ${_accelerometerValues[2]}'),
+            // SizedBox(height: 16.0),
+            // Text('User Accelerometer Data'),
+            // Text('X: ${_userAccelerometerValues[0]}'),
+            // Text('Y: ${_userAccelerometerValues[1]}'),
+            // Text('Z: ${_userAccelerometerValues[2]}'),
+            // SizedBox(height: 16.0),
+            // Text('Gyroscope Data'),
+            // Text('X: ${_gyroscopeValues[0]}'),
+            // Text('Y: ${_gyroscopeValues[1]}'),
+            // Text('Z: ${_gyroscopeValues[2]}'),
+            // SizedBox(height: 16.0),
+            // Text('Magnetometer Data'),
+            // Text('X: ${_magnetometerValues[0]}'),
+            // Text('Y: ${_magnetometerValues[1]}'),
+            // Text('Z: ${_magnetometerValues[2]}'),
+            // SizedBox(height: 16.0),
+            // if (_startLocation != null) ...[
+            //   Text('Latitude: ${_startLocation!.latitude}'),
+            //   Text('Longitude: ${_startLocation!.longitude}'),
+            // ] else
+            //   Text('Location data unavailable'),
+            // SizedBox(height: 16.0),
+            // Text('Light Intensity: $_lightIntensity'),
 
           ],
         ),
@@ -266,7 +394,9 @@ class _FirstPageState extends State<FirstPage> {
       setState(() {
 
       });
-      uploadImage();
+      // uploadImage();
+      Navigator.pushNamed(context, '/second');
+
       // Handle the captured image path, e.g., display it in an ImageView
       print('Image Path: ${imagePath.path}');
     }
@@ -278,10 +408,10 @@ class _FirstPageState extends State<FirstPage> {
     final request = http.MultipartRequest("POST", Uri.parse('https://8bc4-89-143-87-222.eu.ngrok.io/upload'));
     final headers = {"Content-type": "multipart/form-data"};
     request.files.add(
-      http.MultipartFile('image', selectedImage!.readAsBytes().asStream(), selectedImage!.lengthSync(),
-          filename: selectedImage!.path.split("/").last)
+        http.MultipartFile('image', selectedImage!.readAsBytes().asStream(), selectedImage!.lengthSync(),
+            filename: selectedImage!.path.split("/").last)
     );
-    
+
     request.headers.addAll(headers);
     final response = await request.send();
     http.Response res = await http.Response.fromStream(response);
